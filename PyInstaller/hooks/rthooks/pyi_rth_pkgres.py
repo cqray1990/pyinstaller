818838e2448bed2fdb949ba71eaa8c1720b15ffc
Rok Mandeljc <rok.mandeljc@gmail.com>
2020-10-23 11:29:01 +0200

    hooks: pkg_resources: begin implementing custom provider
    Implement PyiFrozenProvider, a simple subclass of NullProvider
    from pkg_resources that provides _has(), _isdir(), and _listdir()
    methods. Their implementations are currently equivalent to the
    ones in DefaultProvider and are filesystem-only.
    
    This should be enough to make pkg_resources.resource_listdir()
    work with the non-egg modules. However, it is not sufficient
    for the conformance test, because it does not implement support
    for embedded resources.

--- PyInstaller/hooks/rthooks/pyi_rth_pkgres.py
+++ PyInstaller/hooks/rthooks/pyi_rth_pkgres.py
@@ -14,9 +14,30 @@ import pkg_resources as res
 from pyimod03_importers import FrozenImporter
 
 
-# To make pkg_resources work with froze moduels we need to set the 'Provider'
+# To make pkg_resources work with frozen modules we need to set the 'Provider'
 # class for FrozenImporter. This class decides where to look for resources
 # and other stuff. 'pkg_resources.NullProvider' is dedicated to PEP302
 # import hooks like FrozenImporter is. It uses method __loader__.get_data() in
 # methods pkg_resources.resource_string() and pkg_resources.resource_stream()
-res.register_loader_type(FrozenImporter, res.NullProvider)
+
+# We subclass the NullProvider and implement _has(), _isdir(), and _listdir(),
+# which are needed for pkg_resources.has_resource(), pkg_resources.resource_isdir(),
+# and pkg_resources.resource_listdir() to work. We cannot use the DefaultProvider,
+# because it provides filesystem-only implementations (and overrides _get()
+# with a filesystem-only one), whereas our provider needs to also support
+# embedded resources.
+
+class PyiFrozenProvider(res.NullProvider):
+    def __init__(self, module):
+        super().__init__(module)
+
+    def _has(self, path):
+        return os.path.exists(path)
+
+    def _isdir(self, path):
+        return os.path.isdir(path)
+
+    def _listdir(self, path):
+        return os.listdir(path)
+
+res.register_loader_type(FrozenImporter, PyiFrozenProvider)

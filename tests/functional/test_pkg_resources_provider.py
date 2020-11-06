6e5afb4bab63f86485e18cdca825d75621156f92
Rok Mandeljc <rok.mandeljc@gmail.com>
2020-11-06 17:02:57 +0100

    tests: test_pkg_resources_provider: build the test egg package on-the-fly
    Build the test egg from the test package source on-the-fly. This saves
    us the hassle of having to manually update the egg each time the
    test package is modified.

--- tests/functional/test_pkg_resources_provider.py
+++ tests/functional/test_pkg_resources_provider.py
@@ -14,6 +14,7 @@
 # ---------------
 import copy
 import os
+import shutil
 
 # Third-party imports
 # -------------------
@@ -22,7 +23,7 @@ import pytest
 # Local imports
 # -------------
 from PyInstaller.utils.tests import importorskip
-from PyInstaller.compat import exec_python_rc
+from PyInstaller.compat import exec_python, exec_python_rc
 
 # Directory with testing modules used in some tests.
 _MODULES_DIR = os.path.join(
@@ -46,25 +47,41 @@ def __exec_python_script(script_filename, *args, pathex=None):
     return exec_python_rc(*cmd, env=env)
 
 
-def __get_test_package_path(package_type):
-    # Same test package, in three different formats
+def __get_test_package_path(package_type, tmpdir, monkeypatch):
+    # Same test package, in two different formats: source package or
+    # zipped egg (built on-the-fly)
+    src_path = os.path.join(_MODULES_DIR,
+                            'pyi_pkg_resources_provider',
+                            'package')
+    # Source package
     if package_type == 'pkg':
-        pathex = os.path.join(_MODULES_DIR,
-                              'pyi_pkg_resources_provider',
-                              'package')
-    elif package_type == 'egg':
-        pathex = os.path.join(_MODULES_DIR,
-                              'pyi_pkg_resources_provider',
-                              'egg-zipped.egg')
+        return src_path
 
-    return pathex
+    # Copy files to a tmpdir for building the egg.
+    dest_path = tmpdir.join('src')
+    shutil.copytree(src_path, dest_path.strpath)
+    monkeypatch.chdir(dest_path)
+
+    # Create an egg from the test package. For debug, show the output of
+    # the egg build.
+    print(exec_python('setup.py', 'bdist_egg'))
+
+    # Obtain the name of the egg, which depends on the Python version.
+    dist_path = dest_path.join('dist')
+    files = os.listdir(dist_path.strpath)
+    assert len(files) == 1
+    egg_name = files[0]
+    assert egg_name.endswith('.egg')
+
+    return dist_path.join(egg_name).strpath
 
 
 @importorskip('pkg_resources')
 @pytest.mark.parametrize('package_type', ['pkg', 'egg'])
-def test_pkg_resources_provider_source(package_type, script_dir):
+def test_pkg_resources_provider_source(package_type, tmpdir, script_dir,
+                                       monkeypatch):
     # Run the source python test script
-    pathex = __get_test_package_path(package_type)
+    pathex = __get_test_package_path(package_type, tmpdir, monkeypatch)
     test_script = 'pyi_pkg_resources_provider.py'
 
     # NOTE: str() is needed for python 3.5
@@ -75,9 +92,10 @@ def test_pkg_resources_provider_source(package_type, script_dir):
 
 @importorskip('pkg_resources')
 @pytest.mark.parametrize('package_type', ['pkg', 'egg'])
-def test_pkg_resources_provider_frozen(pyi_builder, package_type, script_dir):
+def test_pkg_resources_provider_frozen(pyi_builder, package_type, tmpdir,
+                                       script_dir, monkeypatch):
     # Run the test script as a frozen program
-    pathex = __get_test_package_path(package_type)
+    pathex = __get_test_package_path(package_type, tmpdir, monkeypatch)
     test_script = 'pyi_pkg_resources_provider.py'
 
     hooks_dir = os.path.join(_MODULES_DIR,
